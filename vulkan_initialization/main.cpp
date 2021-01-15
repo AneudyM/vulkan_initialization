baff8a1df120a2764427fea14a7727f27131e753
Aneudy Mota <aneudy.motacatalino@outlook.com>
2021-01-15 18:30:07 -0400

    Choosing surface format

--- vulkan_initialization/main.cpp
+++ vulkan_initialization/main.cpp
@@ -447,6 +447,19 @@ private:
 		return indices.isComplete() && extensionsSupported && swapChainAdequate;
 	}
 
+	VkSurfaceFormatKHR chooseSwapSurfaceFormat( const std::vector<VkSurfaceFormatKHR> &availableFormats )
+	{
+		for ( const auto &availableFormat : availableFormats )
+		{
+			if ( availableFormat.format == VK_FORMAT_B8G8R8A8_SRGB && availableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR )
+			{
+				return availableFormat;
+			}
+		}
+
+		return availableFormats[0];
+	}
+
 	/*
 		Functions based on structs above
 	*/
